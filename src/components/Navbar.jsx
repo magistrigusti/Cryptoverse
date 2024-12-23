2294fd32028a62929ad19140380927d1e34270ec
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2024-12-23 13:17:48 +0200

    0.2b

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,45 +1,49 @@
 import React from 'react';
-import { Button, Menu, Typography, Avatar } from 'antd';
+import { Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 import {
   HomeOutlined,
   MoneyCollectOutlined,
   BulbOutlined, 
-  FundOutlined,
-  MenuOutlined
+  FundOutlined
 } from '@ant-design/icons';
 import icon from '../images/cryptocurrency.png';
+
+const items = [
+  {
+    label: <Link to="/">Home</Link>,
+    key: 'home',
+    icon: <HomeOutlined />,
+  },
+  {
+    label: <Link to="/cryptocurrencies">Cryptocurrencies</Link>,
+    key: 'cryptocurrencies',
+    icon: <FundOutlined />,
+  },
+  {
+    label: <Link to="/exchanges">Exchanges</Link>,
+    key: 'exchanges',
+    icon: <MoneyCollectOutlined />,
+  },
+  {
+    label: <Link to="/news">News</Link>,
+    key: 'news',
+    icon: <BulbOutlined />,
+  }
+];
 
 const Navbar = () => {
   return (
     <div className="nav-container">
       <div className="logo-container">
-        <Avatar src={icon} size="lagge" />
-
+        <Avatar src={icon} size="large" />
         <Typography.Title level={2} className="logo">
           <Link to="/">Cryptoverse</Link>
         </Typography.Title>
-
-        <Menu theme="dark">
-          <Menu.Item icon={<HomeOutlined />} >
-            <Link to="/">Home</Link>
-          </Menu.Item>
-
-          <Menu.Item icon={<FundOutlined />}>
-            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
-          </Menu.Item>
-
-          <Menu.Item icon={<MoneyCollectOutlined />} >
-            <Link to="/exchanges">Exchanges</Link>
-          </Menu.Item>
-
-          <Menu.Item icon={<BulbOutlined />} >
-            <Link to="/news">News</Link>
-          </Menu.Item>
-        </Menu>
       </div>
+      <Menu theme="dark" items={items} />
     </div>
-  )
+  );
 }
 
 export default Navbar;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import 'antd/dist/reset.css';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      
+        <App />
+      
     </BrowserRouter>
   </React.StrictMode>
 );
